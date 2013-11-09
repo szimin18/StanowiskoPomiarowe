696c0fb0a149b67a7520028ea8009493e3779f39
Marek Mateja <mateja@student.agh.edu.pl>
2013-11-09 02:10:41 +0100

    Recorder interface

---
+++ Stanowisko/Stanowisko.Rejestrator/Recorder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stanowisko.SharedClasses;
+
+namespace Rejestrator
+{
+    interface Recorder
+    {
+        public void startRecording();
+        public void stopRecording();
+        public Measurement getRecording();
+        public void setPeriod(uint period);
+    }
+}
