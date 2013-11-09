e0454b76705ea59fe83da1e12b22cb1f00dfd45c
Marek Mateja <mateja@student.agh.edu.pl>
2013-11-09 17:54:49 +0100

    Renaming Recorder project, and applying naming connection

---
+++ Stanowisko/Stanowisko.Recorder/IRecorder.cs
@@ -0,0 +1,12 @@
+﻿using Stanowisko.SharedClasses;
+
+namespace Rejestrator
+{
+    interface IRecorder
+    {
+        void startRecording();
+        void stopRecording();
+        Measurement getRecording();
+        void setPeriod(uint period);
+    }
+}
