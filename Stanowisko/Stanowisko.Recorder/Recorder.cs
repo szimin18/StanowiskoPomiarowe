0dd1b2c875d171fb47194da4e08c583258243856
piotr.mikoda <piotr.mikoda@gmail.com>
2013-11-10 17:37:33 +0100

    Recorder class stub

---
+++ Stanowisko/Stanowisko.Recorder/Recorder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stanowisko.SharedClasses;
+using Stanowisko.Symulator;
+
+namespace Rejestrator
+{
+    class Recorder : IRecorder
+    {
+        #region Private Member Variables
+        private const uint defaultPeriod = 1000;
+        private uint period;
+        private IMeasuringDevice measuringDevice;
+        private bool isRecording;
+        private Measurement currentMeasurement;
+        #endregion
+
+        #region Constructors
+        public Recorder(IMeasuringDevice measuringDevice)
+        {
+            this.measuringDevice = measuringDevice;
+            period = defaultPeriod;
+        }
+        #endregion
+
+        #region Public Methods
+        public void startRecording()
+        {
+        }
+
+        public void stopRecording()
+        {
+        }
+
+        public void setPeriod(uint period)
+        {
+            this.period = period;
+        }
+        #endregion
+    }
+}
