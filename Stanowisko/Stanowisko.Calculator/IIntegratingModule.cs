1ccd3ccbea756a74ab4988fa028bae5514267cde
tkwiecien <tomasz.kwiecien1@gmail.com>
2013-11-15 21:16:55 +0100

    Integration interface and class again
    I have no idea why it wasn't committed last time...

---
+++ Stanowisko/Stanowisko.Calculator/IIntegratingModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stanowisko.SharedClasses;
+
+namespace Stanowisko.Calculator
+{
+    public interface IIntegratingModule
+    {
+        double Integrate(List<Sample> samples, int beggining, int end);
+    }
+}
---
+++ Stanowisko/Stanowisko.Calculator/TrapezoidalIntegratingModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stanowisko.SharedClasses;
+
+namespace Stanowisko.Calculator
+{
+    public class TrapezoidalIntegratingModule : IIntegratingModule
+    {
+        public double Integrate(List<Sample> samples, int beggining, int end)
+        {
+            if (samples.Count > 1)
+            {
+                double value = 0.0;
+
+                for (int i = beggining; i < end; ++i)
+                {
+                    value += (samples.ElementAt(i).Value + samples.ElementAt(i + 1).Value);
+                }
+                value *= (samples.ElementAt(1).Time - samples.ElementAt(0).Time) / 2;
+
+                return value;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
