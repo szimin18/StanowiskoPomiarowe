c351f41a22bd8b23a36e32664870b2f9a15868a7
Marek Żarnowski <marek1840@gmail.com>
2013-11-16 10:35:52 +0100

    Added contructor to Sample class

--- Stanowisko/Stanowisko.SharedClasses/Sample.cs
+++ Stanowisko/Stanowisko.SharedClasses/Sample.cs
@@ -11,6 +11,13 @@ namespace Stanowisko.SharedClasses
 
         public double Time { get; private set; }
 
+        public Sample(int id, double value, double time)
+        {
+            Id = id;
+            Value = value;
+            Time = time;
+        }
+
         public Sample(double value, double time)
         {
             Id = Convert.ToInt32(File.ReadAllText("../../SampleID.csv"));
