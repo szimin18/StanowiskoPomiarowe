a2a586e89b991c7d15a9b215fc6e63af6395675f
Marek Żarnowski <marek1840@gmail.com>
2013-11-18 07:53:46 +0100

    Fixed minor naming issue

--- Stanowisko/Stanowisko.SharedClasses/Experiment.cs
+++ Stanowisko/Stanowisko.SharedClasses/Experiment.cs
@@ -53,12 +53,12 @@ namespace Stanowisko.SharedClasses
             if (ms != null) _measurements.RemoveAll(ms.Contains);
         }
 
-        public void AddDescription(string name, string desc)
+        public void AddParameters(string name, string desc)
         {
             _parameters.Add(name, desc);
         }
 
-        public void RemoveDescription(string name)
+        public void RemoveParameter(string name)
         {
             _parameters.Remove(name);
         }
@@ -68,7 +68,7 @@ namespace Stanowisko.SharedClasses
             return new Dictionary<string, string>(_parameters);
         }
 
-        public List<Measurement> getMeasurements()
+        public List<Measurement> GetMeasurements()
         {
             return new List<Measurement>(_measurements);
         }
