dc64c56fcb8152835c2c52651fabb001cbe1eac2
tkwiecien <tomasz.kwiecien1@gmail.com>
2013-12-03 12:02:24 +0100

    Monte Carlo Deletion

--- Stanowisko/Stanowisko.Testy/IntegratingModuleTest.cs
+++ Stanowisko/Stanowisko.Testy/IntegratingModuleTest.cs
@@ -13,84 +13,57 @@ namespace Stanowisko.Testy
         public void TrapezoidalLineIntegrationTest()
         {
             List<Sample> readings = new List<Sample>();
-            for (int i = 0; i < 11; ++i)
+            for (int i = 0; i < 1001; ++i)
             {
                 readings.Add(new Sample(i, i));
             }
             IIntegratingModule target = new TrapezoidalIntegratingModule();
 
-            Assert.AreEqual(target.Integrate(readings, 0, 10), 50.0);
+            Assert.AreEqual(target.Integrate(readings, 0, 1000), 500000.0);
         }
 
         [TestMethod]
         public void TrapezoidalParabolaIntegrationTest()
         {
             List<Sample> readings = new List<Sample>();
-            for (int i = 0; i < 11; ++i)
+            for (int i = 0; i < 1001; ++i)
             {
-                readings.Add(new Sample((-i * (i - 10)), i));
+                readings.Add(new Sample((-i * (i - 1000)), i));
             }
             IIntegratingModule target = new TrapezoidalIntegratingModule();
 
-            double epsilon = 2;
-            double expected = 500.0 / 3.0;
-            Assert.IsTrue(Math.Abs(target.Integrate(readings, 0, 10) - expected) < epsilon);
+            double epsilon = 200;
+            double expected = 500000000.0 / 3.0;
+            double result = target.Integrate(readings, 0, 1000);
+            Assert.IsTrue(Math.Abs(result - expected) < epsilon);
         }
 
         [TestMethod]
         public void SimpsonsLineIntegrationTest()
         {
             List<Sample> readings = new List<Sample>();
-            for (int i = 0; i < 11; ++i)
+            for (int i = 0; i < 1001; ++i)
             {
                 readings.Add(new Sample(i, i));
             }
             IIntegratingModule target = new SimpsonsIntegratingModule();
 
-            Assert.AreEqual(target.Integrate(readings, 0, 10), 50.0);
+            Assert.AreEqual(target.Integrate(readings, 0, 1000), 500000.0);
         }
 
         [TestMethod]
         public void SimpsonsParabolaIntegrationTest()
         {
             List<Sample> readings = new List<Sample>();
-            for (int i = 0; i < 11; ++i)
+            for (int i = 0; i < 1001; ++i)
             {
-                readings.Add(new Sample((-i * (i - 10)), i));
+                readings.Add(new Sample((-i * (i - 1000)), i));
             }
             IIntegratingModule target = new SimpsonsIntegratingModule();
 
             double epsilon = 2;
-            double expected = 500.0 / 3.0;
-            Assert.IsTrue(Math.Abs(target.Integrate(readings, 0, 10) - expected) < epsilon);
-        }
-
-        [TestMethod]
-        public void MonteCarloLineIntegrationTest()
-        {
-            List<Sample> readings = new List<Sample>();
-            for (int i = 0; i < 11; ++i)
-            {
-                readings.Add(new Sample(i, i));
-            }
-            IIntegratingModule target = new MonteCarloIntegratingModule();
-
-            Assert.AreEqual(target.Integrate(readings, 0, 10), 50.0);
-        }
-
-        [TestMethod]
-        public void MonteCarloParabolaIntegrationTest()
-        {
-            List<Sample> readings = new List<Sample>();
-            for (int i = 0; i < 11; ++i)
-            {
-                readings.Add(new Sample((-i * (i - 10)), i));
-            }
-            IIntegratingModule target = new MonteCarloIntegratingModule();
-
-            double epsilon = 2;
-            double expected = 500.0 / 3.0;
-            Assert.IsTrue(Math.Abs(target.Integrate(readings, 0, 10) - expected) < epsilon);
+            double expected = 500000000.0 / 3.0;
+            Assert.IsTrue(Math.Abs(target.Integrate(readings, 0, 1000) - expected) < epsilon);
         }
     }
 }
