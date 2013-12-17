0d6fd736d5191b8860545fa5d047ea64be82039c
tkwiecien <tomasz.kwiecien1@gmail.com>
2013-12-17 12:29:19 +0100

    FactoryMethod for IntegratingModule

---
+++ Stanowisko/Stanowisko.Calculator/IIntegratingModuleCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stanowisko.Calculator
+{
+    interface IIntegratingModuleCreator
+    {
+        public TrapezoidalIntegratingModule TrapezoidalIntegratingModuleCreate();
+        public SimpsonsIntegratingModule SimpsonsIntegratingModuleCreate();
+    }
+}
---
+++ Stanowisko/Stanowisko.Calculator/IntegratingModuleCreator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stanowisko.Calculator
+{
+    class IntegratingModuleCreator : IIntegratingModuleCreator
+    {
+        TrapezoidalIntegratingModule trapezoidal = null;
+        SimpsonsIntegratingModule simpsons = null;
+
+        public IIntegratingModule TrapezoidalIntegratingModuleCreate()
+        {
+            if (trapezoidal == null)
+                trapezoidal = new TrapezoidalIntegratingModule();
+            return trapezoidal;
+        }
+
+        public IIntegratingModule SimpsonsIntegratingModuleCreate()
+        {
+            if (simpsons == null)
+                simpsons = new SimpsonsIntegratingModule();
+            return simpsons;
+        }
+    }
+}
