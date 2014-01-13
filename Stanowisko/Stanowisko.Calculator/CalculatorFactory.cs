9abde5cb7c09e771214004e1624f8c42b779230c
tkwiecien <tomasz.kwiecien1@gmail.com>
2014-01-13 02:17:20 +0100

    Calculator factory

---
+++ Stanowisko/Stanowisko.Calculator/CalculatorFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stanowisko.SharedClasses;
+
+namespace Stanowisko.Calculator
+{
+    class CalculatorFactory : ICalculatorFactory
+    {
+        public IMeasurementCalculator CreateCalculator(Measurement measurement, IIntegratingModule integrator)
+        {
+            return new MeasurementCalculator(measurement, integrator);
+        }
+    }
+}
---
+++ Stanowisko/Stanowisko.Calculator/ICalculatorFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stanowisko.SharedClasses;
+
+namespace Stanowisko.Calculator
+{
+    class ICalculatorFactory
+    {
+        public IMeasurementCalculator CreateCalculator(Measurement measurement, IIntegratingModule integrator);
+    }
+}
